cb927ee06b19d4e8f0ecce5e53521c950109f1b5
brrik0677 <leo.luvs.footwork@gmail.com>
2024-12-19 21:45:58 +0900

    Send item update

--- main.py
+++ main.py
@@ -19,19 +19,18 @@ with st.form(key='iken_form', clear_on_submit=True):
     #送信ボタン
     submit_btn = st.form_submit_button('投稿')
     if submit_btn:
-        st.text(f'投稿完了しました！')
-        st.text(f'投稿内容確認')
-        st.text(f'{comment}')
 
         url = "https://pythonapi-egwh.onrender.com/postdata/"
         data = {
             "roll": yakusyoku,
             "data": comment
         }
-
         response = requests.post(url, json=data)
 
-        #役職、投稿内容、レスポンス確認用[200]で成功　消しても良い
-        print(f'選択された役職: {yakusyoku}')
-        print(f'投稿された内容: {comment}')
-        print(response)
+        if response.status_code == 200:
+            st.success(f'投稿完了しました！')
+            st.text(f'投稿内容確認')
+            st.text(f'{comment}')
+        else:
+            st.error('エラーが発生しました。再度お試しください。')
+            print(f'エラー: {response.status_code} - {response.text}')
